8f0603cfb290935d5296c16d10b3bff6685ba7ae
s-gasaro <s.gasaro1@alustudent.com>
2025-04-05 21:02:36 +0000

    p-sqaure

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,4 +1,5 @@
-kes a URL and an email address, sends a POST request to the passed URL
+#!/usr/bin/python3
+"""Takes a URL and an email address, sends a POST request to the passed URL
 with the email as a parameter and finally displays the body of the response
 """
 
