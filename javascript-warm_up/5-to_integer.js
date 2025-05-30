abb344fe77200c3a331b91d809199ea049b7b670
Sarah Gasaro <s.gasaro1@alustudent.com>
2025-05-30 14:33:03 -0700

    DTFM

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-const number = parseInt(firstArg, 10);
-if (!isNaN(number)) {
-  console.log(`My number: ${number}`);
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
   console.log('Not a number');
 }
+
